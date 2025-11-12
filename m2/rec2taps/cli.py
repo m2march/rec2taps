f4c83e26866f9f6114b6a4ec3e4b457a383a7e6d
March <m2.march@gmail.com>
2025-11-12 14:56:27 -0500

    Parameter parsing for new mode

--- m2/rec2taps/cli.py
+++ m2/rec2taps/cli.py
@@ -1,10 +1,16 @@
+import sys
+import logging
+logging.basicConfig(filename=sys.stderr)
+logging.getLogger().setLevel(logging.INFO)
+
 import argparse
 import os
-import sys
 import m2.rec2taps
-import logging
+import re
 from m2.rec2taps import defaults
 from m2.rec2taps import errors
+
+FILE_CHANNEL_RE = re.compile(r'([\w\.]+):(\d+)')
 
 def rec2taps():
     parser = argparse.ArgumentParser(
@@ -14,13 +20,20 @@ def rec2taps():
                      'to auditory stimuli" (Miguel et. al. 2020).')
     )
     
-    parser.add_argument('stimuli_file', type=str,
+    parser.add_argument('stimuli', type=str, metavar='stimuli_file(:channel)',
                         help='audio file of the stimuli')
-    parser.add_argument('recording_file', type=str,
+    parser.add_argument('recording', type=str, 
+                        metavar='recording_file(:loopback_channel)',
                         help=('audio file of the experiment recording. Should '
                               'have two channels, one with a loopback '
                               'recording of the stimuli and another one with '
                               'the signal from the input device.')
+                       )
+    parser.add_argument('tapping_files', type=str,
+                        metavar='tapping_files:channel',
+                        nargs='*',
+                        help=('(optional) sequence of files and channel of interest'
+                              'from which to extract taps synchronized to the stimuli')
                        )
     parser.add_argument('-d', dest='distance',
                         type=int, default=defaults.DEFAULT_DISTANCE, 
@@ -45,32 +58,69 @@ def rec2taps():
                               'an argument to define the output filename.'))
     args = parser.parse_args()
 
-    if not os.path.isfile(args.stimuli_file):
-        print('{} does not refer to a file.'.format(args.stimuli_file))
-        sys.exit()
-    if not os.path.isfile(args.recording_file):
-        print('{} does not refer to a file.'.format(args.recording_file))
-        sys.exit()
-
     if args.verbose:
-        logging.basicConfig(level=logging.DEBUG, 
-                            format="Debug: {message}",
-                            style='{')
+        logging.getLogger().setLevel(logging.DEBUG)
 
-    try:
-        peaks = m2.rec2taps.extract_peaks(args.stimuli_file,
-                                          args.recording_file,
-                                          args.distance,
-                                          args.prominence,
-                                          args.debug_plot,
-                                          args.invert_input
-                                         )
-    except errors.Rec2TapsError as r2te:
-        print(r2te, file=sys.stderr)
-        sys.exit()
+    logging.debug('Input paramters:' + str([args.stimuli, args.recording, args.tapping_files]))
+    if args.stimuli.find(':') > -1 or len(args.tapping_files) > 0:
+        logging.info('Processing files in INDIVIDUAL CHANNEL MODE')
 
-    for p in peaks:
-        print(p)
+        can_proceed = True
+
+        for s in [args.stimuli, args.recording] + args.tapping_files:
+            if FILE_CHANNEL_RE.match(s) is None:
+                logging.error(f' {s} does not have the expected format `file_name:channel_number`')
+                can_proceed = False
+
+        if len(args.tapping_files) == 0:
+            logging.error(' No tapping files were provided.')
+            can_proceed = False
+
+        if not can_proceed:
+            logging.error(' Files were not properly input. Stopping.')
+            sys.exit()
+
+        stimuli_file, stimuli_channel = FILE_CHANNEL_RE.match(args.stimuli).groups()
+        loopback_file, loopback_channel = FILE_CHANNEL_RE.match(args.recording).groups()
+        tapping_files_channels = [FILE_CHANNEL_RE.match(t).groups() for t in args.tapping_files]
+
+        audio_files = [stimuli_file, loopback_file] + [
+            f for f, c in tapping_files_channels
+        ]
+
+        for file in audio_files:
+            if not os.path.isfile(file):
+                logging.error(f' Audio file could not be found: {file}')
+                can_proceed = False
+        
+        if not can_proceed:
+            logging.error(' Files were not properly input. Stopping.')
+            sys.exit()
+    
+
+    else:
+        logging.info('Processing files in STIMULI-RECORDING FILE MODE')
+        if not os.path.isfile(args.stimuli):
+            print('{} does not refer to a file.'.format(args.stimuli))
+            sys.exit()
+        if not os.path.isfile(args.recording):
+            print('{} does not refer to a file.'.format(args.recording))
+            sys.exit()
+
+        try:
+            peaks = m2.rec2taps.extract_peaks(args.stimuli,
+                                              args.recording,
+                                              args.distance,
+                                              args.prominence,
+                                              args.debug_plot,
+                                              args.invert_input
+                                             )
+        except errors.Rec2TapsError as r2te:
+            print(r2te, file=sys.stderr)
+            sys.exit()
+
+        for p in peaks:
+            print(p)
 
 if __name__ == '__main__':
     main()
